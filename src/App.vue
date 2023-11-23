7854faefeeda2a159f34b87b94555dd2320d01c4
Yahya Mardini <120016906+gityahco@users.noreply.github.com>
2023-11-23 21:48:22 +0300

    making the grid

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Gird from './components/grid.vue'
+</script>
+
+<template>
+  <Gird/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/grid.vue
@@ -0,0 +1,32 @@
+
+<!-- Grid.vue -->
+<script lang="ts">
+export default {
+  data() {
+    const ROWS = 30
+    const COLS = 30
+    return {
+      grid: Array.from({ length: ROWS }, () => Array(COLS).fill('')),
+    };
+  },
+};
+</script>
+
+<template>
+
+    <div class="grid">
+      <div v-for="(row, rowIndex) in grid" :key="rowIndex" class="flex">
+        <div
+          v-for="(_, colIndex) in row"
+          :key="colIndex"
+          class="h-5 w-5 border border-gray-300"
+        >
+        </div>
+      </div>
+    </div>
+
+  </template>
+
+  <style scoped>
+
+  </style>
